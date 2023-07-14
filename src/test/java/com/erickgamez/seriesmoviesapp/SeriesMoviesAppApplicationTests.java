456dd17f316336df605a1ed7cddb299151ade799
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-14 13:46:18 -0600

    Configuración inicial del proyecto y creación del mismo

---
+++ src/test/java/com/erickgamez/seriesmoviesapp/SeriesMoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickgamez.seriesmoviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesMoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
