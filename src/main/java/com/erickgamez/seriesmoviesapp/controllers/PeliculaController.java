82a3176e9dacc97ddaabf8f4a4311b4f2bcc34ee
erickgamez15 <erickgamezsanchez@gmail.com>
2023-07-18 20:41:45 -0600

    Maquetación del header del sistema

--- src/main/java/com/erickgamez/seriesmoviesapp/controllers/PeliculaController.java
+++ src/main/java/com/erickgamez/seriesmoviesapp/controllers/PeliculaController.java
@@ -1,6 +1,7 @@
 package com.erickgamez.seriesmoviesapp.controllers;
 
 import com.erickgamez.seriesmoviesapp.entities.Pelicula;
+import com.erickgamez.seriesmoviesapp.services.IGeneroService;
 import com.erickgamez.seriesmoviesapp.services.IPeliculaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PeliculaController {
 
     private final IPeliculaService peliculaService;
+    private final IGeneroService generoService;
     Pelicula pelicula;
 
-    public PeliculaController(IPeliculaService peliculaService){
+    public PeliculaController(IPeliculaService peliculaService, IGeneroService generoService){
         this.peliculaService = peliculaService;
+        this.generoService = generoService;
         pelicula = new Pelicula();
     }
 
@@ -28,6 +31,7 @@ public class PeliculaController {
     @GetMapping("/pelicula/{id}")
     public String edit(@PathVariable(name = "id") Long id, Model model){
         model.addAttribute("titulo", "Editar Película");
+        model.addAttribute("genero", generoService.findAll());
         model.addAttribute("pelicula", pelicula);
         return "pelicula";
     }
